475cf36134254ae0534c52c0aefbb60b0a3196f3
lakshay-png <lakshaykarwa1234@gmail.com>
2026-05-16 22:34:49 +0530

    feat: added PDF ingestion and text chunking pipeline

--- app/backend/text_splitter.py
+++ app/backend/text_splitter.py
@@ -1,6 +1,8 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def split_documents(documents):
+
+    # PDF text extraction and chunking logic
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200
